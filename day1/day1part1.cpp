e5a190395df200bc21389728711b371d37930fb3
x1A0hz <119912418+x1A0hz@users.noreply.github.com>
2022-12-07 19:15:55 +0800

    Update day1part1.cpp

--- day1/day1part1.cpp
+++ day1/day1part1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <cstdlib>
 using namespace std;
 
@@ -8,7 +11,7 @@ using namespace std;
 int main() {
   // Create a variable to store the total calories for 1 elf
   int totalfor1elf;
-  // Create a variable to store the most calories of an elf 
+  // Create a variable to store the most calories of an elve 
   int mostCal;
   // Create a text string, which is used to output the text file
   string calories;
@@ -17,20 +20,21 @@ int main() {
   ifstream file("caloriesinput.txt");
   // Use a while loop together with the getline() function to read the file line by line
   while (getline (file, calories)) {
+    string eachtotal[] = {to_string(totalfor1elf)};
+    if (stoi(eachtotal[0]) > mostCal) {
+    mostCal = stoi(eachtotal[0]) ; 
+    }
     if (calories.empty()) {
       totalfor1elf = 0;  //to reset & calculate total calories for next elf
-    } else { 
+    } 
+    else { 
       // stoi(calories) to convert string data type to int
       totalfor1elf += stoi(calories); 
       }
-    
-  string eachtotal[] = {to_string(totalfor1elf)};
-  if (stoi(eachtotal[0]) > mostCal) {
-    mostCal = stoi(eachtotal[0]) ; 
-    }
   }  
   
   cout << mostCal << endl;
+  
   
   // Close the file
   file.close();
